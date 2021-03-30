9df7108279f97f97a609d5ef65d5dafd979c2b0d
jideoyelayo1 <41443216+jideoyelayo1@users.noreply.github.com>
2021-03-30 13:39:02 +0100

    Create primenumbercalc.py

---
+++ primenumbercalc.py
@@ -0,0 +1,20 @@
+def primeNums(n):
+    nums = list(range(1,n))
+    ans = []
+    prime = 0
+    while(len(nums) > 1):
+        if nums[1] == 1:
+            nums.pop(0)
+        i = 0
+        prime = nums[1]
+        ans.append(nums[1])
+        while i < len(nums):
+            if nums[i]%prime == 0:
+                nums.pop(i)
+            i+=1
+    return ans
+
+
+n = int(input("Enter n: "))
+print(primeNums(n))
+print("The number of prime numbers are: "+len(primeNums(n)))
